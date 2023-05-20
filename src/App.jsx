b06f35d48d8a9350f7bddbcf015ceb244658cc93
Mineiroc4 <rafaellmineirovarela@hotmail.com>
2023-05-19 21:57:29 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import HelloFunctionComponent from './components/HelloFunctionComponent'
+
+function App() {
+
+  return (
+    <HelloFunctionComponent name='Rafael'/> 
+  )
+}
+
+export default App
---
+++ src/Components/HelloFunctionComponent.jsx
@@ -0,0 +1,4 @@
+function HelloFunctionComponent(props){
+    return( <h1>Hello {props.name} !!</h1>)
+}
+export default HelloFunctionComponent
